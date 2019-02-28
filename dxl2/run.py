14e34092ac9c2743f7923c561c7299b2192168de
xykl0n3 <sishirmehta98@gmail.com>
2019-02-28 07:32:56 +0530

    updated run and added motion

--- dxl2/run.py
+++ dxl2/run.py
@@ -2,33 +2,50 @@ import dxl2
 import time
 from dxl2 import register
 
+
+
+
 conn = dxl2.Connection("/dev/tty.usbserial-FT1SF3Q6")
 conn.open_port()
 
- m = dxl2.Motor(conn, 4, dxl2.MotorType.AX)
+#  m = dxl2.Motor(conn, 4, dxl2.MotorType.AX)
 # print(m.read(register.Instruction.PRESENT_POSITION))
 
 # for i in range(1, 13):
 #     m = dxl2.Motor(conn, i, dxl2.MotorType.AX)
+#     d = dxl2.Motor(conn,i-1,dxl2.MotorType.AX)
 #     m.write(register.Instruction.MOVING_SPEED, 100)
 #     # print(i, m.read(register.Instruction.PRESENT_POSITION))
-#     if i == 2:
-#         m.write(register.Instruction.GOAL_POSITION, 205)
-#     elif i % 3 != 1:
+    
+#     if i % 3 != 1:
 #         m.write(register.Instruction.GOAL_POSITION, 450)
 #     else:
 #         m.write(register.Instruction.GOAL_POSITION, 512)
 
 
-# # print(m.read(register.Instruction.GOAL_POSITION))
 
-# # from dynamixel_sdk import *
+####### gait ######
 
-# # port_handler = PortHandler("/dev/tty.usbserial-FT1SF3Q6")
-# # port_handler.openPort()
-# # packet_handler = PacketHandler(1.0)
-# # print(packet_handler.ping(port_handler, 2))""
+def stand():
 
-m.write(register.Instruction.GOAL_POSITION,715)
-time.sleep(1)
-m.write(register.Instruction.GOAL_POSITION,250)
+    text_file = open("stand.txt", "r")
+    pos= []
+    for val in text_file.read().split(','):
+    pos.append(int(val))
+    text_file.close()
+    for i in range (1,13):
+        m = dxl2.Motor(conn, i, dxl2.MotorType.AX)
+        m.write(register.Instruction.GOAL_POSITION, pos)
+        time.sleep(0.5)
+
+
+def walk():
+
+    text_file = open("walk.txt", "r")
+    pos= []
+    for val in text_file.read().split(','):
+    pos.append(int(val))
+    text_file.close()
+
+
+
