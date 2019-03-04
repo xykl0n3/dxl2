ba41256170ae3c20ed183556ee744b5f4e74a89e
xykl0n3 <sishirmehta98@gmail.com>
2019-03-04 16:27:24 +0530

    sorted files

---
+++ dxl2/biped/run.py
@@ -0,0 +1,56 @@
+import dxl2
+import time
+from dxl2 import register
+from read import read_file
+
+
+
+
+conn = dxl2.Connection("/dev/ttyUSB0")
+conn.open_port()
+
+# for i in range (1,11):
+#         m = dxl2.Motor(conn, i, dxl2.MotorType.AX)
+#         print(m.read(register.Instruction.PRESENT_POSITION))        
+
+
+####### gait ######
+
+
+
+
+def write():
+
+        pos = read_file('motion/stand.txt')
+        for list in pos:
+                for i in range (1,11):
+                        m = dxl2.Motor(conn, i, dxl2.MotorType.AX)
+                        m.write(register.Instruction.MOVING_SPEED,40)
+                        
+                        m.write(register.Instruction.GOAL_POSITION, list[i-1])
+
+def read():     
+        for i in range (1,11):
+                m = dxl2.Motor(conn, i, dxl2.MotorType.AX)
+                print(m.read(register.Instruction.PRESENT_POSITION))
+        
+# for i in range (7,9):
+#         m = dxl2.Motor(conn, i, dxl2.MotorType.AX)
+#        # print(m.read(register.Instruction.PRESENT_POSITION))
+#         m.write(register.Instruction.MOVING_SPEED,40)
+#         m.write(register.Instruction.GOAL_POSITION,512)
+       
+#m.write(register.Instruction.TORQUE_ENABLE,0)
+
+
+
+# def walk():
+
+#     text_file = open("walk.txt", "r")
+#     pos= []
+#     for val in text_file.read().split(','):
+#     pos.append(int(val))
+#     text_file.close()
+
+
+
